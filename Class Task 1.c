0014d28526a1538d41f4a32b0d3440449f392e42
Ramiq Ehsan <k250863@nu.edu.pk>
2025-11-13 13:05:59 +0500

    Add files via upload

---
+++ Class Task 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+// TASK 1
+
+
+int main(){
+
+char word[4][10]={
+	"Apple",
+	"Grapes",
+	"Banana",
+	"Orranges"
+};
+
+int i =0;
+for(i; i<4; i++){
+	puts(word[i]);
+}
+
+printf("\n\n");
+
+strcpy(word[1],"Mango");
+
+i=0;
+int j= 0;
+for(i; i<4; i++){
+	puts(word[i]);
+	
+	for(j;word[i][j]!='\0';j++){
+		printf("%c\n",word[i][j]);
+	}
+	j=0;
+	
+	printf("word count: %d\n\n",strlen(word[i]));
+	
+	
+}
+
+
+
+return 0;
+}
+
